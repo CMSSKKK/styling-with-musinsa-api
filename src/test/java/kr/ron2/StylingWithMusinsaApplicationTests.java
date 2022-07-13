f2477460f5445aba59d0e462cf70eea10c3f52eb
Minseok-Choi <cms05041@gmail.com>
2022-07-13 16:24:27 +0900

    feat: 프로젝트 초기 셋팅 및 readme 필수 요구사항 작성

---
+++ src/test/java/kr/ron2/StylingWithMusinsaApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ron2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StylingWithMusinsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
